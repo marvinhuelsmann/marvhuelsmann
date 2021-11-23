cc3038f0fe0b7f6967c664eaf88726795f0713c5
Marvin Hülsmann <mac@marvhuelsmann.de>
2021-11-23 22:47:02 +0100

    add local mail to ContactForm.js

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -60,14 +60,16 @@ export default function ContactForm() {
                 </div>
             </div>
             <p className={"text-gray-400 dark:text-gray-300 text-md pb-4 mb-2 text-center items-center flex-col justify-center"}>
-                Bis jetzt kannst du mich über dieses Kontaktformular noch nicht erreichen!
+                Bis jetzt kannst du mich über dieses Kontaktformular noch nicht erreichen, wenn du mich <br/>troztdem kontaktieren willst
+                schreibe eine E-Mail an <a className={"text-blue-400 hover:text-blue-500"} href={"mailto:kontakt@marvhuelsmann.com"}>kontakt@marvhuelsmann.com</a>!
             </p>
             <div className={"right"}>
                 <h1 className="text-black dark:text-white text-center mt-10 font-bold xl:text-5xl md:text-4xl text-2xl">
                     Nachricht
                 </h1>
                 <p className={"text-gray-600 dark:text-gray-300 text-xl pb-4 mb-2 text-center items-center flex-col justify-center"}>
-                    Falls du mir etwas wichtiges mitteilen möchtest oder ein gutes und cooles<br/> Projekt im Sinn hast ist dies der perfekte Weg.
+                    Falls du mir etwas wichtiges mitteilen möchtest oder ein gutes und cooles<br/> Projekt im Sinn hast
+                    ist dies der perfekte Weg.
                 </p>
             </div>
         </div>
