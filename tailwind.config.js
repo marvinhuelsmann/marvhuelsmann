425df5d3fbf8305cd429a35bbafb5fb78f70db37
Marvin Hülsmann <mac@marvhuelsmann.de>
2022-02-12 21:47:05 +0100

    add new image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: ['./pages/*.{js,ts,jsx,tsx}', './components/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       fontFamily: {
