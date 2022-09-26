885bce975c11e934578eaff1e2c838d635c55e71
Marvin Hülsmann <mac@marvhuelsmann.de>
2022-09-26 18:22:04 +0200

    remove animations

--- pages/_app.js
+++ pages/_app.js
@@ -4,19 +4,7 @@ import {motion} from "framer-motion";
 function marvinhuelsmann({Component, pageProps}) {
     return (
         <div>
-            <motion.main
-                animate={{
-                    scale: [1.1, 1],
-                }}
-                exit="exit" // Exit state (used later) to variants.exit
-                transition={{type: 'linear'}} // Set the transition to linear
-                className="xl:block md:block hidden"
-            >
                 <Component {...pageProps} />
-            </motion.main>
-            <div className="xl:hidden md:hidden block">
-                <Component {...pageProps} />
-            </div>
         </div>
     )
 }
