4f65212878b61f485ac186c9f1557b4876c16a79
marvinhuelsmann <mail@marvhuelsmann.com>
2022-12-08 22:25:32 +0100

    fix one sheet button in light mode

--- components/home/Footer.js
+++ components/home/Footer.js
@@ -18,7 +18,7 @@ export default function Footer() {
             <div className={"flex -mt-3 pb-5 justify-center"}>
                 <a target={"_blank"} href={"https://www.trnsp.ml/_next/image?url=https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Ftransportier.appspot.com%2Fo%2Fuploads%252F1670450044175.png%3Falt%3Dmedia%26token%3Ded13b195-6780-4731-a922-a8b494ca062c&w=1920&q=75"} className={"text-3xl"}>
                     <button type="button"
-                            className="text-white bg-black items-center hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded px-2 py-1.5 mr-1 mb-1 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-600 dark:border-gray-600 text-xl">
+                            className="dark:text-white dark:bg-black bg-gray-400 text-gray-900 items-center hover:bg-gray-500 dark:hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-300 rounded px-2 py-1.5 mr-1 mb-1 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-600 dark:border-gray-600 text-xl">
                         <img className={"inline xl:h-6 md:h-7 h-6 pr-1"} src={MemoEmoji.src}
                              alt={"Memo"}/> One Sheet
                     </button>
