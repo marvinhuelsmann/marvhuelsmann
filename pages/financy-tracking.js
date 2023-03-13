fab95207c3bc3e23cfef76226cb0cc4d01fa9cb8
marvinhuelsmann <mail@marvhuelsmann.com>
2023-03-13 16:58:35 +0100

    add financy privacy policy

---
+++ pages/financy-tracking.js
@@ -0,0 +1,13 @@
+
+export default function FinancyTracking() {
+    return (
+        <div>
+            <h1 className={"font-bold text-3xl"}>
+                Privacy Policy
+            </h1>
+            <p className={"text-xl"}>
+                Financy does not collect any type of data for tracking. Financy collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
+            </p>
+        </div>
+    );
+}
