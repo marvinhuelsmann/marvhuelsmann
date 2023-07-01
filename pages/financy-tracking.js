75a336085bb686523d1672318d684a7ea65393bf
marvinhuelsmann <mail@marvhuelsmann.com>
2023-07-01 21:33:10 +0200

    add seo

--- pages/financy-tracking.js
+++ pages/financy-tracking.js
@@ -2,11 +2,11 @@
 export default function FinancyTracking() {
     return (
         <div>
-            <h1 className={"pl-2 pt-4font-bold text-3xl"}>
+            <h1 className={"pl-2 pt-4 font-bold text-3xl"}>
                 Privacy Policy
             </h1>
-            <p className={"text-xl pl-4 pr-4"}>
-                Financy does not collect any type of data for tracking. Financy collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
+            <p className={"text-xl pt-4 pl-4 pr-4"}>
+                Financy & JourneyMate does not collect any type of data for tracking. Financy & JourneyMate collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
             </p>
         </div>
     );
