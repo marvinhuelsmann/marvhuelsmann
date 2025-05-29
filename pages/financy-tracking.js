cc82edd373b70a8a7e5aa0f83c523e591654677e
Marvin Hülsmann <mrntv64@gmail.com>
2025-05-29 02:05:57 +0200

    update financy-tracking.js

--- pages/financy-tracking.js
+++ pages/financy-tracking.js
@@ -6,7 +6,7 @@ export default function FinancyTracking() {
                 Privacy Policy
             </h1>
             <p className={"text-xl pt-4 pl-4 pr-4"}>
-                Financy & JourneyMate does not collect any type of data for tracking. Financy & JourneyMate collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
+                DoThis & JourneyMate does not collect any type of data for tracking. DoThis & JourneyMate collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
             </p>
         </div>
     );
