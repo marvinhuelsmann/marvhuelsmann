a155d591ff961d4ac0f20c3bfa48adc07275c27a
Marvin Hülsmann <mac@marvhuelsmann.de>
2026-06-10 19:17:24 +0200

    Update financy-tracking.js

--- pages/financy-tracking.js
+++ pages/financy-tracking.js
@@ -6,7 +6,7 @@ export default function FinancyTracking() {
                 Privacy Policy
             </h1>
             <p className={"text-xl pt-4 pl-4 pr-4"}>
-                DoThis & JourneyMate does not collect any type of data for tracking. DoThis & JourneyMate collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
+                DoThis & Procto does not collect any type of data for tracking. DoThis & Procto collects anonymous usage data for private analytics, which is not used for marketing, tracking, advertisement or anything alike.
             </p>
         </div>
     );
